6d7d92ca99ebd55ba6a688a35b2b106797fb0b82
puppywimy <eric250@naver.com>
2026-07-16 12:38:24 +0900

    refactor: BaseEntity 마이그레이션

---
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.kt
@@ -0,0 +1,28 @@
+package com.back.global.jpa.entity
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.IDENTITY
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    final var id: Int? = 0
+        private set
+
+    @CreatedDate
+    final var createDate: LocalDateTime? = null
+        private set
+
+    @LastModifiedDate
+    final var modifyDate: LocalDateTime? = null
+        private set
+}
