17f92b8f7d749b50e83aaf05f985dce76f58ba63
puppywimy <eric250@naver.com>
2026-07-16 12:11:20 +0900

    refactor: Post Service 마이그레이션

---
+++ src/main/java/com/back/domain/post/post/service/PostService.kt
@@ -0,0 +1,28 @@
+package com.back.domain.post.post.service
+
+import com.back.domain.member.member.entity.Member
+import com.back.domain.post.post.entity.Post
+import com.back.domain.post.post.repository.PostRepository
+import org.springframework.stereotype.Service
+import java.util.Optional
+
+@Service
+class PostService(
+    private val postRepository: PostRepository,
+) {
+    fun count(): Long = postRepository.count()
+
+    fun findById(id: Int): Optional<Post> = postRepository.findById(id)
+
+    fun modify(post: Post, title: String, content: String) {
+        post.title = title
+        post.content = content
+    }
+
+    fun write(author: Member, title: String, content: String): Post {
+        val post = Post(author, title, content)
+        postRepository.save(post)
+
+        return post
+    }
+}
