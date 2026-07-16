87839b931fb734463e8721f567e64334e82adb8d
puppywimy <eric250@naver.com>
2026-07-16 12:14:11 +0900

    refactor: Member Entity 마이그레이션

---
+++ src/main/java/com/back/domain/member/member/entity/Member.kt
@@ -0,0 +1,16 @@
+package com.back.domain.member.member.entity
+
+import com.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+
+@Entity
+class Member(
+    @Column(unique = true)
+    var username: String?,
+    var password: String?,
+    var nickname: String?,
+) : BaseEntity() {
+    override fun toString(): String =
+        "Member(username=$username, password=$password, nickname=$nickname)"
+}
