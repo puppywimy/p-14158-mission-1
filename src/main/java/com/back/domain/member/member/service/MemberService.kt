159637400a0a2f28b55896ea20779476fd57f7fa
puppywimy <eric250@naver.com>
2026-07-16 12:16:05 +0900

    refactor: Member Service 마이그레이션

---
+++ src/main/java/com/back/domain/member/member/service/MemberService.kt
@@ -0,0 +1,15 @@
+package com.back.domain.member.member.service
+
+import com.back.domain.member.member.entity.Member
+import com.back.domain.member.member.repository.MemberRepository
+import org.springframework.stereotype.Service
+
+@Service
+class MemberService(
+    private val memberRepository: MemberRepository,
+) {
+    fun count(): Long = memberRepository.count()
+
+    fun join(username: String, password: String, nickname: String): Member =
+        memberRepository.save(Member(username, password, nickname))
+}
